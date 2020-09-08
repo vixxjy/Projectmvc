eb4337463278921585d83619deadd732e4c572bc
smilez <vixxjy@gmail.com>
2020-09-08 22:52:20 +0100

    creating category model and adding to db

--- Book.DataAccess/Data/ApplicationDbContext.cs
+++ Book.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProjectMVC.Models;
 
 namespace ProjectMVC.DataAccess.Data
 {
@@ -12,5 +13,7 @@ namespace ProjectMVC.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
